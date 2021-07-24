12e2121407fc09d270d5c648e751531666b84937
mlfernandez <mlfernandez@gmail.com>
2021-07-24 23:36:01 +0200

    first commit

---
+++ router.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+/* const movieRouter = require("./routes/moviesRouter");
+const seriesRouter = require("./routes/seriesRouter");
+const usersRouter = require('./routes/usersRouter');
+const orderRouter = require('./routes/ordersRouters')
+const loginRouter = require('./routes/loginRouter')
+
+router.use("/movies", movieRouter);
+router.use("/series", seriesRouter);
+router.use('/users', usersRouter);
+router.use('/orders', orderRouter);
+router.use('/login', loginRouter); */
+
+
+module.exports = router;
